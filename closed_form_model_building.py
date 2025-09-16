4360480f1e904097339bbfbf0889eb3a4957bfeb
modelverseml <83335447+modelverseml@users.noreply.github.com>
2025-09-15 22:02:38 -0400

    Update closed_form_model_building.py

--- closed_form_model_building.py
+++ closed_form_model_building.py
@@ -8,29 +8,23 @@ class ClosedFormRegressionModel:
         self.X_train = X_train
         self.y_train = y_train
         self.feature_names = list(X_train.columns)
-
         
     def build_model(self):
 
         X = self.X_train.to_numpy()
         Y = self.y_train.to_numpy()
-
         X = np.hstack([np.ones((X.shape[0], 1)), X])
-        
         self.feature_names = ["Intercept"] + self.feature_names
 
         try :
-            
             X_transpose = np.linalg.inv(X.T @ X )
 
         except : 
-            
             X_transpose = np.linalg.pinv(X.T @ X )
 
         self.cofficients = X_transpose@(X.T @ Y)
         
 
-    
     def get_prameters(self):
 
         
@@ -40,9 +34,7 @@ class ClosedFormRegressionModel:
         })
         
         return coef_df
-        
 
-    
     def predict(self, X):
 
         X = np.hstack([np.ones((X.shape[0], 1)), X])
